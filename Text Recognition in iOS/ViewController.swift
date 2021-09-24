83be8437110eb2dd72e8ee8e1082118e80855d48
hamednrg <hamednajirg@gmail.com>
2021-09-24 17:48:37 +0330

    Initial Commit

---
+++ Text Recognition in iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Text Recognition in iOS
+//
+//  Created by Hamed on 7/2/1400 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
